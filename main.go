17cb7f3819f5fbf6fcad5f3fc50ac264dcad03a9
mijime <07cc0110+gthb@gmail.com>
2015-07-08 14:46:37 +0900

    use remarshal

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
-	"github.com/BurntSushi/toml"
-	"gopkg.in/yaml.v2"
+	"github.com/mijime/merje/remarshal"
 	"io"
 	"io/ioutil"
 	"os"
@@ -14,70 +11,100 @@ import (
 	"text/template"
 )
 
-var decode = flag.String("decode", "json", "input format")
-var format = flag.String("format", "{{json .}}", "output format")
-var output = flag.String("output", "", "output path")
+var inputFormatString = flag.String("if", "ext", "input format")
+var outputFormatString = flag.String("of", "json", "output format")
+var outputPath = flag.String("o", "", "output path")
 
 func main() {
 	flag.Parse()
 	inputs := flag.Args()
 
 	var result interface{}
+	var inputFormat remarshal.Format
+	var inputBuffer []byte
+
+	switch *inputFormatString {
+	case "yaml":
+		inputFormat = remarshal.YAML
+	case "toml":
+		inputFormat = remarshal.TOML
+	case "json":
+		inputFormat = remarshal.JSON
+	default:
+		inputFormat = remarshal.Unknown
+	}
 
 	if len(inputs) <= 0 {
-		b, err := ioutil.ReadAll(os.Stdin)
+		inputBuffer, err := ioutil.ReadAll(os.Stdin)
 
 		if err != nil {
 			panic(err)
 		}
 
-		result = typeDecode(b, *decode)
+		result, err = remarshal.Unmarshal(inputBuffer, inputFormat)
 
 	} else {
 		for _, input := range inputs {
-			var b []byte
-			var decoder string
+			var err error
+			var _inputFormat remarshal.Format
 
 			if isFilePath(input) {
-				var err error
-				b, err = ioutil.ReadFile(input)
+				inputBuffer, err = ioutil.ReadFile(input)
 
 				if err != nil {
 					panic(err)
 				}
+
 				switch path.Ext(input) {
 				case ".yml", ".yaml":
-					decoder = "yaml"
+					_inputFormat = remarshal.YAML
 				case ".tml", ".toml":
-					decoder = "toml"
+					_inputFormat = remarshal.TOML
 				case ".json":
-					decoder = "json"
+					_inputFormat = remarshal.JSON
 				default:
-					decoder = *decode
+					_inputFormat = remarshal.Unknown
 				}
 			} else {
-				b = []byte(input)
-				decoder = *decode
+				inputBuffer = []byte(input)
 			}
 
-			dataBuf := typeDecode(b, decoder)
-			result = mergeInterface(result, dataBuf)
+			if inputFormat == remarshal.Unknown {
+				dataBuf, _ := remarshal.Unmarshal(inputBuffer, _inputFormat)
+				result = mergeInterface(result, dataBuf)
+
+			} else {
+				dataBuf, _ := remarshal.Unmarshal(inputBuffer, inputFormat)
+				result = mergeInterface(result, dataBuf)
+			}
 		}
 	}
 
 	var writer io.Writer
+	var outputFormat string
 
-	if *output == "" {
+	if *outputPath == "" {
 		writer = os.Stdout
 	} else {
 		var err error
-		writer, err = os.Create(*output)
+		writer, err = os.Create(*outputPath)
 		if err != nil {
 			panic(err)
 		}
 	}
 
-	tmpl, err := buildTemplate(*format)
+	switch *outputFormatString {
+	case "yaml":
+		outputFormat = "{{yaml .}}"
+	case "toml":
+		outputFormat = "{{toml .}}"
+	case "json":
+		outputFormat = "{{json .}}"
+	default:
+		outputFormat = *outputFormatString
+	}
+
+	tmpl, err := buildTemplate(outputFormat)
 	if err != nil {
 		panic(err)
 
@@ -122,81 +149,40 @@ func mergeHash(prev, curr map[string]interface{}) map[string]interface{} {
 	return prev
 }
 
-func typeDecode(b []byte, decoder string) interface{} {
-	var dataBuf interface{}
-
-	switch decoder {
-	case "yaml":
-		err := yaml.Unmarshal(b, &dataBuf)
-		if err != nil {
-			panic(err)
-		}
-	case "toml":
-		err := toml.Unmarshal(b, &dataBuf)
-		if err != nil {
-			panic(err)
-		}
-	default:
-		err := json.Unmarshal(b, &dataBuf)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	return dataBuf
-}
-
-func jsonEncode(v interface{}) string {
-	buf, err := json.Marshal(v)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return string(buf)
-}
-
-func yamlEncode(v interface{}) string {
-	buf, err := yaml.Marshal(v)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return string(buf)
-}
-
-func tomlEncode(v interface{}) string {
-	buf := new(bytes.Buffer)
-	enc := toml.NewEncoder(buf)
-	err := enc.Encode(v)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return buf.String()
-}
-
-func buildTemplate(format string) (*template.Template, error) {
+func buildTemplate(outputFormatString string) (*template.Template, error) {
 	funcMap := template.FuncMap{
 		"split":   strings.Split,
 		"join":    strings.Join,
 		"replace": strings.Replace,
 		"base":    path.Base,
 		"dir":     path.Dir,
-		"json":    jsonEncode,
-		"yaml":    yamlEncode,
-		"toml":    tomlEncode,
+		"json":    jsonMarshal,
+		"yaml":    yamlMarshal,
+		"toml":    tomlMarshal,
 	}
 
-	if _, err := os.Stat(format); err != nil {
-		return template.New("variable").Funcs(funcMap).Parse(format)
+	if _, err := os.Stat(outputFormatString); err != nil {
+		return template.New("variable").Funcs(funcMap).Parse(outputFormatString)
 
 	} else {
-		name := path.Base(format)
-		return template.New(name).Funcs(funcMap).ParseFiles(format)
+		name := path.Base(outputFormatString)
+		return template.New(name).Funcs(funcMap).ParseFiles(outputFormatString)
 	}
+}
+
+func jsonMarshal(data interface{}) (result string, err error) {
+	b, err := remarshal.Marshal(data, remarshal.JSON)
+	return string(b), err
+}
+
+func yamlMarshal(data interface{}) (result string, err error) {
+	b, err := remarshal.Marshal(data, remarshal.YAML)
+	return string(b), err
+}
+
+func tomlMarshal(data interface{}) (result string, err error) {
+	b, err := remarshal.Marshal(data, remarshal.TOML)
+	return string(b), err
 }
 
 func isFilePath(i string) bool {
